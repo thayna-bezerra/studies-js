82cc0a943b4fa8485b63a746f6635f666009d4b2
thayna-bezerra <arraisthayna2106@gmail.com>
2022-12-11 15:01:38 -0300

    eventos

--- stage05-explorer/jogo-adivinhacao/main.js
+++ stage05-explorer/jogo-adivinhacao/main.js
@@ -1,21 +1,37 @@
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+
 const randomNumber = Math.round(Math.random() * 10);
 let totalAttempts = 1;
 
-
-function handleClick(event){
-  event.preventDefault()
+//função callback
+function handleTryClick(event){
+  event.preventDefault() //nao enviar o form (n faça o padrao)
 
   console.log(randomNumber);
   const inputNumber = document.querySelector("#inputNumber")
 
   if(Number(inputNumber.value) == randomNumber){
-    document.querySelector(".screen1").classList.add("hide")
-    document.querySelector(".screen2").classList.remove("hide")
+    screen1.classList.add("hide")
+    screen2.classList.remove("hide")
     
     document
       .querySelector(".screen2 h2")
-      .innerText = `acertou em ${totalAttempts}`
+      .innerText = `Acertou em ${totalAttempts} tentativas`
   }
   
+  inputNumber.value = "";
   totalAttempts++;
 }
+
+//Eventos
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', function(){
+  screen1.classList.remove("hide")
+  screen2.classList.add("hide")
+
+  totalAttempts = 1;
+})
