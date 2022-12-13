fb7ec1a2c7e92023e9afe4f6796d6d092bcfdb95
thayna-bezerra <arraisthayna2106@gmail.com>
2022-12-13 12:07:20 -0300

    estruturando os dados do modal

--- stage05-explorer/projeto-imc/script.js
+++ stage05-explorer/projeto-imc/script.js
@@ -3,10 +3,22 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
+//const modalWrapper = document.querySelector('.modal-wrapper')
+//const modalMessage = document.querySelector('.modal .title span')
+//const modalBtnClose = document.querySelector('.modal button.close')
 
+const Modal = {
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  btnClose: document.querySelector('.modal button.close'),
+
+  open(){
+    Modal.wrapper.classList.add('open')
+  }, 
+  close(){
+    Modal.wrapper.classList.remove('open')
+  }
+}
 
 form.onsubmit = function(event) {
   event.preventDefault();
@@ -17,12 +29,11 @@ form.onsubmit = function(event) {
   const result = IMC(weight, height);
   const message = `Seu IMC é de ${result}`
 
-  modalMessage.innerText = message
-  modalWrapper.classList.add('open')
+  Modal.message.innerText = message
+  Modal.open()
 }
 
-modalBtnClose.onclick = () => 
-  modalWrapper.classList.remove('open')
+Modal.btnClose.onclick = () => Modal.close()
 
 function IMC(weight, height) {
   return (weight / ((height / 100) ** 2)).toFixed(2)
