39fdcbfd8fdeb972cfa9d59b9d567ecd7c0ca5fb
thayna-bezerra <arraisthayna2106@gmail.com>
2022-12-13 14:55:20 -0300

    aprendendo sobre es6 modules

---
+++ stage05-explorer/projeto-imc/js/script.js
@@ -0,0 +1,30 @@
+import { Modal } from "./modal.js"
+import {AlertError} from "./alert-error.js"
+import {IMC, notNumber} from "./utils.js"
+
+// variables
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+form.onsubmit = function(event) {
+  event.preventDefault();
+
+  const weight = inputWeight.value
+  const height = inputHeight.value 
+
+  const showAlertError = notNumber(weight) || notNumber(height)
+
+  if(showAlertError){
+    AlertError.open()
+    return;
+  }
+
+  AlertError.close()
+
+  const result = IMC(weight, height);
+  const message = `Seu IMC é de ${result}`
+
+  Modal.message.innerText = message
+  Modal.open()
+}
